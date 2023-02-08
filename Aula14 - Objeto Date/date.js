0b1d3b577d9ad03915b4aed74e48bb24a7c8e340
Davi Miguel <davi.miguel2020@gmail.com>
2023-02-08 09:07:49 -0300

    Conceitos Obj Date

---
+++ Aula14 - Objeto Date/date.js
@@ -0,0 +1,4 @@
+// const data = new Date(); //Dia de Hoje
+// const data = new Date(2023, 1, 08, 07, 50, 55); //Definindo a data específica
+const data = new Date(1675858000000); //Milissegundos a partir da data base
+console.log(data.toString());
