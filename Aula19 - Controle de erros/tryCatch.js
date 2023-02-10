ee9d35529a335bf792ff3a63141c90381753633a
Davi Miguel <davi.miguel2020@gmail.com>
2023-02-10 17:03:20 -0300

    Aula 19 - Finalizada

--- Aula19 - Controle de erros/tryCatch.js
+++ Aula19 - Controle de erros/tryCatch.js
@@ -1,5 +1,15 @@
+const soma = (x, y) => {
+	if (typeof x !== 'number' || typeof y !== 'number') {
+		throw `Erro na soma ${x} + ${y} | As variaveis precisam ser números`;
+	}
+	return x + y;
+};
+
 try {
-	console.log(num);
+	console.log(soma(2, 32));
+	console.log(soma('a', 32));
 } catch (err) {
-	console.log('A variavel não existe');
+	console.log(err);
+} finally {
+	console.log('Função Finalizada!');
 }
