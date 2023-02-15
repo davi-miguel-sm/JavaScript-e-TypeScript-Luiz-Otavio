79e458e68142e4cdf19f789e13082403afedb2e5
Davi Miguel <davi.miguel2020@gmail.com>
2023-02-14 23:11:19 -0300

    Base do Projeto da Lista de Tarefas Iniciado

---
+++ Aula20 - Projeto Lista de tarefas/assets/js/main.js
@@ -0,0 +1,42 @@
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+btnTarefa.addEventListener('click', (e) => {
+	if (!inputTarefa.value) return;
+	criaTarefa(inputTarefa.value);
+	limpaInput();
+});
+
+inputTarefa.addEventListener('keypress', (e) => {
+	if (e.keyCode === 13) {
+		if (!inputTarefa.value) return;
+		criaTarefa(inputTarefa.value);
+		limpaInput();
+	}
+});
+
+function criaElemento(elem) {
+	const elemento = document.createElement(elem);
+	return elemento;
+}
+
+function criaTarefa(textoInput) {
+	const li = criaElemento('li');
+	li.innerText = textoInput;
+	tarefas.appendChild(li);
+	limparTarefa(li);
+}
+
+function limpaInput() {
+	inputTarefa.value = '';
+	inputTarefa.focus();
+}
+
+function limparTarefa(elem) {
+	elem.innerText += ' ';
+	const botaoApagar = criaElemento('button');
+	botaoApagar.setAttribute('class', 'apagar');
+	botaoApagar.innerText = 'Apagar';
+	elem.appendChild(botaoApagar);
+}
