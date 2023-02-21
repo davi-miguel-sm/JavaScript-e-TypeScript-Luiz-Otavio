d5558a2ef731e24b24ea73643e9d2a54c345e69d
Davi Miguel <davi.miguel2020@gmail.com>
2023-02-20 23:51:52 -0300

    Funções geradoras finalizadas

---
+++ Aula24 - Função Geradora/geradora.js
@@ -0,0 +1,32 @@
+function* geradora1() {
+	yield 'Valor 1';
+	yield 'Valor 2';
+	yield 'Valor 3';
+}
+
+const g1 = geradora1();
+
+for (let valor of g1) {
+	console.log(valor);
+}
+
+function* geradora2() {
+	let i = 0;
+	while (true) {
+		yield i;
+		i++;
+	}
+}
+
+const g2 = geradora2();
+
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
