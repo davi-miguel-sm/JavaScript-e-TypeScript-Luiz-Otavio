a99116320afd2358f57100485dec663c9512ed19
Davi Miguel <davi.miguel2020@gmail.com>
2023-02-24 23:29:38 -0300

    Introdução a type annotation

---
+++ LangTS/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  trailingComa: 'all',
+  singleQuote: true,
+  printWidth: 85,
+  tabWidth: 2,
+  endOfLine: 'auto',
+};
---
+++ LangTS/Aula01 - Tipos/typeAnnotaion.ts
@@ -0,0 +1,23 @@
+/* eslint-disable */
+
+//Tipos básicos (Existe inferência de tipos nesses casos)
+let nome: string = 'Davi'; //Qlqr string `` '' "";
+let idade: number = 0b1010; //10, 1.57, -5.55, 0xf00d, 0o7744, 0b1010
+let adulto: boolean = true; // true ou false
+let simbolo: symbol = Symbol('*');
+let big: bigint = 100n;
+
+//Arrays
+let arrayDeNumeros: Array<number> = [1, 2, 3, 4];
+let arrayDeNumeros2: number[] = [1, 2, 3, 4];
+let arrayDeStrings: Array<string> = ['1', '2', '3'];
+let arrayDeStrings2: string[] = ['1', '2', '3'];
+
+//Objetos
+let pessoa: { nome: string; idade: number; adulto?: boolean } = {
+  nome: 'Davi Miguel',
+  idade: 21,
+  adulto: true,
+};
+
+console.log(pessoa.adulto);
