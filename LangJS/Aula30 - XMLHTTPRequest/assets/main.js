ea501941c6622b13954cabca2268a1a495274b6b
Davi Miguel <davi.miguel2020@gmail.com>
2024-10-31 19:12:04 -0300

    Aula 30 - XMLHttpRequest

--- LangJS/Aula30 - XMLHTTPRequest/assets/main.js
+++ LangJS/Aula30 - XMLHTTPRequest/assets/main.js
@@ -1,13 +1,43 @@
 const request = (obj) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open(obj.method, obj.url, true);
-  xhr.send();
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true);
+    xhr.send();
 
-  xhr.addEventListener('load', () => {
-    if (xhr.status >= 200 && xhr.status < 300) {
-      obj.success(xhr.responseText);
-    } else {
-      obj.error(xhr.statusText);
-    }
+    xhr.addEventListener('load', () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.statusText);
+      }
+    });
   });
 };
+
+const listAllTagA = document.querySelectorAll('a');
+
+for (a of listAllTagA) {
+  a.addEventListener('click', (event) => {
+    event.preventDefault();
+    carregaPagina(event.target);
+  });
+}
+
+async function carregaPagina(element) {
+  let href = element.getAttribute('href');
+
+  const objConfig = {
+    method: 'GET',
+    url: href,
+  };
+  try {
+    const response = await request(objConfig);
+    carregaResultado(response);
+  } catch (error) {
+    carregaResultado(`Foi retornado o erro ${error}`);
+  }
+}
+
+function carregaResultado(text) {
+  document.querySelector('.resultado').innerHTML = text;
+}
